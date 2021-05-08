e5cd224f1f5bc21720171f468f570f70d16a2704
Nithin Sai K J <nithins674@gmail.com>
2021-05-08 16:58:56 +0530

    dynamic routing done

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,27 +1,8 @@
-import Head from 'next/head'
 import Layout from '../../components/layout'
-import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-
+import Head from 'next/head'
+import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
-
-
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
-    return {
-        props: {
-            postData
-        }
-    }
-}
-
-export async function getStaticPaths() {
-    const paths = getAllPostIds()
-    return {
-        paths,
-        fallback: false
-    }
-}
 
 export default function Post({ postData }) {
     return (
@@ -38,4 +19,21 @@ export default function Post({ postData }) {
             </article>
         </Layout>
     )
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
 }
